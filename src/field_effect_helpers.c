f6ed78acc18f19e7b980352433a407a48ec6c827
Pawkkie <moog272@gmail.com>
2023-09-09 14:29:36 -0400

    Reflection fix?

--- src/field_effect_helpers.c
+++ src/field_effect_helpers.c
@@ -123,7 +123,7 @@ void LoadSpecialReflectionPalette(struct Sprite *sprite)
 	}
 	reflectionPalette.data = gReflectionPaletteBuffer;
 	reflectionPalette.tag = GetSpritePaletteTagByPaletteNum(sprite->oam.paletteNum) + 0x1000;
-	LoadSpritePalette(&reflectionPalette);
+	LoadSpritePalette_HandleDayNight(&reflectionPalette, TRUE);
 	sprite->oam.paletteNum = IndexOfSpritePaletteTag(reflectionPalette.tag);
 	UpdatePaletteGammaType(sprite->oam.paletteNum, GAMMA_ALT);
 	UpdateSpritePaletteWithWeather(sprite->oam.paletteNum);
