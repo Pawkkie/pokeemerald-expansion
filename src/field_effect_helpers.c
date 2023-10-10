0623c54877a7092ebfea0131455cc64003601e9e
Pawkkie <moog272@gmail.com>
2023-10-10 12:16:36 -0400

    Reflection fix cleanup

--- src/field_effect_helpers.c
+++ src/field_effect_helpers.c
@@ -123,7 +123,7 @@ void LoadSpecialReflectionPalette(struct Sprite *sprite)
 	}
 	reflectionPalette.data = gReflectionPaletteBuffer;
 	reflectionPalette.tag = GetSpritePaletteTagByPaletteNum(sprite->oam.paletteNum) + 0x1000;
-	LoadSpritePalette_HandleDayNight(&reflectionPalette, TRUE);
+	LoadSpritePaletteDayNight(&reflectionPalette);
 	sprite->oam.paletteNum = IndexOfSpritePaletteTag(reflectionPalette.tag);
 	UpdatePaletteGammaType(sprite->oam.paletteNum, GAMMA_ALT);
 	UpdateSpritePaletteWithWeather(sprite->oam.paletteNum);
