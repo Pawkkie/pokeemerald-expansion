957358b706f2fc66093d470a2e37c38e6d1bafbc
Pawkkie <moog272@gmail.com>
2023-11-10 22:48:37 -0500

    Merge fixes yikes

--- src/field_effect_helpers.c
+++ src/field_effect_helpers.c
@@ -207,6 +207,7 @@ extern const struct SpritePalette gSpritePalette_ArrowEmotionsFieldEffect;
 
 u8 CreateWarpArrowSprite(void)
 {
+    LoadFieldEffectPalette_(FLDEFFOBJ_ARROW, FALSE);
     u8 spriteId = CreateSpriteAtEnd(gFieldEffectObjectTemplatePointers[FLDEFFOBJ_ARROW], 0, 0, 82);
     if (spriteId != MAX_SPRITES)
     {
@@ -273,6 +274,7 @@ u32 FldEff_Shadow(void)
 {
     u8 objectEventId = GetObjectEventIdByLocalIdAndMap(gFieldEffectArguments[0], gFieldEffectArguments[1], gFieldEffectArguments[2]);
     const struct ObjectEventGraphicsInfo *graphicsInfo = GetObjectEventGraphicsInfo(gObjectEvents[objectEventId].graphicsId);
+    LoadFieldEffectPalette_(sShadowEffectTemplateIds[graphicsInfo->shadowSize], FALSE);
     u8 spriteId = CreateSpriteAtEnd(gFieldEffectObjectTemplatePointers[sShadowEffectTemplateIds[graphicsInfo->shadowSize]], 0, 0, 148);
     if (spriteId != MAX_SPRITES)
     {
@@ -504,7 +506,6 @@ u32 FldEff_SnowTallGrass(void)
     s16 y;
     u8 spriteId;
     struct Sprite *sprite;
-
     x = gFieldEffectArguments[0];
     y = gFieldEffectArguments[1];
     SetSpritePosToOffsetMapCoords(&x, &y, 8, 8);
@@ -520,7 +521,6 @@ u32 FldEff_SnowTallGrass(void)
         sprite->sMapNum = gFieldEffectArguments[4]; // Also sLocalId
         sprite->sMapGroup = gFieldEffectArguments[5];
         sprite->sCurrentMap = gFieldEffectArguments[6];
-
         if (gFieldEffectArguments[7])
             SeekSpriteAnim(sprite, 4); // Skip to end of anim
     }
@@ -588,117 +588,7 @@ u32 FldEff_JumpSnowTallGrass(void)
         sprite->coordOffsetEnabled = TRUE;
         sprite->oam.priority = gFieldEffectArguments[3];
         sprite->sElevation = gFieldEffectArguments[2];
-        sprite->sFldEff = FLDEFF_JUMP_SNOW_TALL_GRASS;
-    }
-    return 0;
-}
-
-u8 FindSnowTallGrassFieldEffectSpriteId(u8 localId, u8 mapNum, u8 mapGroup, s16 x, s16 y)
-{
-    struct Sprite *sprite;
-    u8 i;
-
-    for (i = 0; i < MAX_SPRITES; i ++)
-    {
-        if (gSprites[i].inUse)
-        {
-            sprite = &gSprites[i];
-            if (sprite->callback == UpdateSnowTallGrassFieldEffect
-                && (x == sprite->sX && y == sprite->sY)
-                && localId == (u8)(sprite->sLocalId)
-                && mapNum == (sprite->sMapNum & 0xFF)
-                && mapGroup == sprite->sMapGroup)
-                return i;
-        }
-    }
-    return MAX_SPRITES;
-}
-
-u32 FldEff_SnowTallGrass(void)
-{
-    u8 spriteId;
-    s16 x = gFieldEffectArguments[0];
-    s16 y = gFieldEffectArguments[1];
-    SetSpritePosToOffsetMapCoords(&x, &y, 8, 8);
-    spriteId = CreateSpriteAtEnd(gFieldEffectObjectTemplatePointers[FLDEFFOBJ_SNOW_TALL_GRASS], x, y, 0);
-    if (spriteId != MAX_SPRITES)
-    {
-        struct Sprite *sprite = &gSprites[spriteId];
-        sprite->coordOffsetEnabled = TRUE;
-        sprite->oam.priority = gFieldEffectArguments[3];
-        sprite->sElevation = gFieldEffectArguments[2];
-        sprite->sX = gFieldEffectArguments[0];
-        sprite->sY = gFieldEffectArguments[1];
-        sprite->sMapNum = gFieldEffectArguments[4]; // Also sLocalId
-        sprite->sMapGroup = gFieldEffectArguments[5];
-        sprite->sCurrentMap = gFieldEffectArguments[6];
-
-        if (gFieldEffectArguments[7])
-            SeekSpriteAnim(sprite, 4); // Skip to end of anim
-    }
-    return 0;
-}
-
-void UpdateSnowTallGrassFieldEffect(struct Sprite *sprite)
-{
-    u8 metatileBehavior;
-    u8 localId;
-    u8 objectEventId;
-    u8 mapNum = sprite->sCurrentMap >> 8;
-    u8 mapGroup = sprite->sCurrentMap;
-
-    if (gCamera.active && (gSaveBlock1Ptr->location.mapNum != mapNum || gSaveBlock1Ptr->location.mapGroup != mapGroup))
-    {
-        sprite->sX -= gCamera.x;
-        sprite->sY -= gCamera.y;
-        sprite->sCurrentMap = ((u8)gSaveBlock1Ptr->location.mapNum << 8) | (u8)gSaveBlock1Ptr->location.mapGroup;
-    }
-    localId = sprite->sLocalId;
-    mapNum = sprite->sMapNum;
-    mapGroup = sprite->sMapGroup;
-    metatileBehavior = MapGridGetMetatileBehaviorAt(sprite->sX, sprite->sY);
-
-
-    if (TryGetObjectEventIdByLocalIdAndMap(localId, mapNum, mapGroup, &objectEventId)
-     || !MetatileBehavior_IsSnowTallGrass(metatileBehavior)
-     || (sprite->sObjectMoved && sprite->animEnded))
-    {
-        FieldEffectStop(sprite, FLDEFF_SNOW_TALL_GRASS);
-    }
-    else
-    {
-        // Check if the object that triggered the effect has moved away
-        objectEvent = &gObjectEvents[objectEventId];
-        if ((objectEvent->currentCoords.x != sprite->sX
-          || objectEvent->currentCoords.y != sprite->sY)
-        && (objectEvent->previousCoords.x != sprite->sX
-         || objectEvent->previousCoords.y != sprite->sY))
-            sprite->sObjectMoved = TRUE;
-
-        // Metatile behavior var re-used as subpriority
-        metatileBehavior = 0;
-        if (sprite->animCmdIndex == 0)
-            metatileBehavior = 4;
-
-        UpdateObjectEventSpriteInvisibility(sprite, FALSE);
-        UpdateGrassFieldEffectSubpriority(sprite, sprite->sElevation, metatileBehavior);
-    }
-}
-
-u32 FldEff_JumpSnowTallGrass(void)
-{
-    u8 spriteId;
-    struct Sprite *sprite;
-
-    SetSpritePosToOffsetMapCoords((s16 *)&gFieldEffectArguments[0], (s16 *)&gFieldEffectArguments[1], 8, 12);
-    spriteId = CreateSpriteAtEnd(gFieldEffectObjectTemplatePointers[FLDEFFOBJ_JUMP_SNOW_TALL_GRASS], gFieldEffectArguments[0], gFieldEffectArguments[1], 0);
-    if (spriteId != MAX_SPRITES)
-    {
-        sprite = &gSprites[spriteId];
-        sprite->coordOffsetEnabled = TRUE;
-        sprite->oam.priority = gFieldEffectArguments[3];
-        sprite->sElevation = gFieldEffectArguments[2];
-        sprite->sFldEff = FLDEFF_JUMP_SNOW_TALL_GRASS;
+        sprite->sJumpFldEff = FLDEFF_JUMP_SNOW_TALL_GRASS;
     }
     return 0;
 }
@@ -857,24 +747,6 @@ u32 FldEff_SnowTracks(void)
     return 0;
 }
 
-u32 FldEff_SnowTracks(void)
-{
-    u8 spriteId;
-    struct Sprite *sprite;
-
-    SetSpritePosToOffsetMapCoords((s16 *)&gFieldEffectArguments[0], (s16 *)&gFieldEffectArguments[1], 8, 8);
-    spriteId = CreateSpriteAtEnd(gFieldEffectObjectTemplatePointers[FLDEFFOBJ_SNOW_TRACKS], gFieldEffectArguments[0], gFieldEffectArguments[1], gFieldEffectArguments[2]);
-    if (spriteId != MAX_SPRITES)
-    {
-        struct Sprite *sprite = &gSprites[spriteId];
-        sprite->coordOffsetEnabled = TRUE;
-        sprite->oam.priority = gFieldEffectArguments[3];
-        sprite->sFldEff = FLDEFF_SAND_FOOTPRINTS;
-        StartSpriteAnim(sprite, gFieldEffectArguments[4]);
-    }
-    return 0;
-}
-
 u32 FldEff_DeepSandFootprints(void)
 {
     u8 spriteId;
@@ -898,24 +770,6 @@ u32 FldEff_BikeTireTracks(void)
 
     SetSpritePosToOffsetMapCoords((s16 *)&gFieldEffectArguments[0], (s16 *)&gFieldEffectArguments[1], 8, 8);
     spriteId = CreateSpriteAtEnd(gFieldEffectObjectTemplatePointers[FLDEFFOBJ_BIKE_TIRE_TRACKS], gFieldEffectArguments[0], gFieldEffectArguments[1], gFieldEffectArguments[2]);
-    if (spriteId != MAX_SPRITES)
-    {
-        struct Sprite *sprite = &gSprites[spriteId];
-        sprite->coordOffsetEnabled = TRUE;
-        sprite->oam.priority = gFieldEffectArguments[3];
-        sprite->sFldEff = FLDEFF_BIKE_TIRE_TRACKS;
-        StartSpriteAnim(sprite, gFieldEffectArguments[4]);
-    }
-    return spriteId;
-}
-
-u32 FldEff_SnowBikeTireTracks(void)
-{
-    u8 spriteId;
-    struct Sprite *sprite;
-
-    SetSpritePosToOffsetMapCoords((s16 *)&gFieldEffectArguments[0], (s16 *)&gFieldEffectArguments[1], 8, 8);
-    spriteId = CreateSpriteAtEnd(gFieldEffectObjectTemplatePointers[FLDEFFOBJ_SNOW_BIKE_TIRE_TRACKS], gFieldEffectArguments[0], gFieldEffectArguments[1], gFieldEffectArguments[2]);
     if (spriteId != MAX_SPRITES)
     {
         struct Sprite *sprite = &gSprites[spriteId];
