pret/pokeemerald

Couple things for 1.8 release (#4274)
* Couple things for 1.8 release

* revert EFFECT_VARY_POWER_BASED_ON_HP change

* Fix comment

---------

Co-authored-by: Eduardo Quezada <eduardo602002@gmail.com>

test/battle/ability/dancer.c
@@ -33,3 +33,23 @@ SINGLE_BATTLE_TEST("Dancer can copy Teeter Dance")
         ANIMATION(ANIM_TYPE_MOVE, MOVE_TEETER_DANCE, opponent);
     }
 }
+
+DOUBLE_BATTLE_TEST("Dancer can copy Teeter Dance and confuse both opposing targets")
+{
+    KNOWN_FAILING; // Fails because copied move that targets both opposing mons, targets only one when copied by Dancer
+    GIVEN {
+        ASSUME(gMovesInfo[MOVE_TEETER_DANCE].danceMove == TRUE);
+        PLAYER(SPECIES_WOBBUFFET)
+        PLAYER(SPECIES_WYNAUT)
+        OPPONENT(SPECIES_ORICORIO) { Ability(ABILITY_DANCER); Item(ITEM_LUM_BERRY); }
+        OPPONENT(SPECIES_SLOWPOKE) { Ability(ABILITY_OWN_TEMPO); }
+    } WHEN {
+        TURN { MOVE(playerLeft, MOVE_TEETER_DANCE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_TEETER_DANCE, playerLeft);
+        ABILITY_POPUP(opponentLeft, ABILITY_DANCER);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_TEETER_DANCE, opponentLeft);
+        MESSAGE("Wobbuffet became confused!");
+        MESSAGE("Wynaut became confusef!");
+    }
+}

Pawkkie/pokeemerald-expansion

test/battle/ability/dancer.c
@@ -33,3 +33,23 @@ SINGLE_BATTLE_TEST("Dancer can copy Teeter Dance")
         ANIMATION(ANIM_TYPE_MOVE, MOVE_TEETER_DANCE, opponent);
     }
 }
+
+DOUBLE_BATTLE_TEST("Dancer can copy Teeter Dance and confuse both opposing targets")
+{
+    KNOWN_FAILING; // Fails because copied move that targets both opposing mons, targets only one when copied by Dancer
+    GIVEN {
+        ASSUME(gBattleMoves[MOVE_TEETER_DANCE].danceMove == TRUE);
+        PLAYER(SPECIES_WOBBUFFET)
+        PLAYER(SPECIES_WYNAUT)
+        OPPONENT(SPECIES_ORICORIO) { Ability(ABILITY_DANCER); Item(ITEM_LUM_BERRY); }
+        OPPONENT(SPECIES_SLOWPOKE) { Ability(ABILITY_OWN_TEMPO); }
+    } WHEN {
+        TURN { MOVE(playerLeft, MOVE_TEETER_DANCE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_TEETER_DANCE, playerLeft);
+        ABILITY_POPUP(opponentLeft, ABILITY_DANCER);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_TEETER_DANCE, opponentLeft);
+        MESSAGE("Wobbuffet became confused!");
+        MESSAGE("Wynaut became confusef!");
+    }
+}