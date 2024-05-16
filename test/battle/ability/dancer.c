pret/pokeemerald

Fixes ability dancer not targeting multiply targets (#4437)
* Fixes ability dancer not targeting multiply targets

* remove file

test/battle/ability/dancer.c
@@ -36,11 +36,11 @@ SINGLE_BATTLE_TEST("Dancer can copy Teeter Dance")
 
 DOUBLE_BATTLE_TEST("Dancer can copy Teeter Dance and confuse both opposing targets")
 {
-    KNOWN_FAILING; // Fails because copied move that targets both opposing mons, targets only one when copied by Dancer
     GIVEN {
         ASSUME(gMovesInfo[MOVE_TEETER_DANCE].danceMove == TRUE);
+        ASSUME(gItemsInfo[ITEM_LUM_BERRY].holdEffect == HOLD_EFFECT_CURE_STATUS);
         PLAYER(SPECIES_WOBBUFFET)
-        PLAYER(SPECIES_WYNAUT)
+        PLAYER(SPECIES_WYNAUT) { Item(ITEM_LUM_BERRY); }
         OPPONENT(SPECIES_ORICORIO) { Ability(ABILITY_DANCER); Item(ITEM_LUM_BERRY); }
         OPPONENT(SPECIES_SLOWPOKE) { Ability(ABILITY_OWN_TEMPO); }
     } WHEN {
@@ -50,6 +50,6 @@ DOUBLE_BATTLE_TEST("Dancer can copy Teeter Dance and confuse both opposing targe
         ABILITY_POPUP(opponentLeft, ABILITY_DANCER);
         ANIMATION(ANIM_TYPE_MOVE, MOVE_TEETER_DANCE, opponentLeft);
         MESSAGE("Wobbuffet became confused!");
-        MESSAGE("Wynaut became confusef!");
+        MESSAGE("Wynaut became confused!");
     }
 }

Pawkkie/pokeemerald-expansion

test/battle/ability/dancer.c
@@ -36,11 +36,11 @@ SINGLE_BATTLE_TEST("Dancer can copy Teeter Dance")
 
 DOUBLE_BATTLE_TEST("Dancer can copy Teeter Dance and confuse both opposing targets")
 {
-    KNOWN_FAILING; // Fails because copied move that targets both opposing mons, targets only one when copied by Dancer
     GIVEN {
         ASSUME(gBattleMoves[MOVE_TEETER_DANCE].danceMove == TRUE);
+        ASSUME(gItems[ITEM_LUM_BERRY].holdEffect == HOLD_EFFECT_CURE_STATUS);
         PLAYER(SPECIES_WOBBUFFET)
-        PLAYER(SPECIES_WYNAUT)
+        PLAYER(SPECIES_WYNAUT) { Item(ITEM_LUM_BERRY); }
         OPPONENT(SPECIES_ORICORIO) { Ability(ABILITY_DANCER); Item(ITEM_LUM_BERRY); }
         OPPONENT(SPECIES_SLOWPOKE) { Ability(ABILITY_OWN_TEMPO); }
     } WHEN {
@@ -50,6 +50,6 @@ DOUBLE_BATTLE_TEST("Dancer can copy Teeter Dance and confuse both opposing targe
         ABILITY_POPUP(opponentLeft, ABILITY_DANCER);
         ANIMATION(ANIM_TYPE_MOVE, MOVE_TEETER_DANCE, opponentLeft);
         MESSAGE("Wobbuffet became confused!");
-        MESSAGE("Wynaut became confusef!");
+        MESSAGE("Wynaut became confused!");
     }
 }