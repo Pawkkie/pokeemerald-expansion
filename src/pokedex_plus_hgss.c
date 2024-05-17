c35f9410d8704f1bc9905936f1d9e59e736549c6
Pawkkie <moog272@gmail.com>
2024-05-16 21:32:41 -0400

    Reset HGSS dex to classic after merging Rave's branch

--- src/pokedex_plus_hgss.c
+++ src/pokedex_plus_hgss.c
@@ -338,7 +338,7 @@ static const u16* sDexPalettes[HGSS_COLOR_COUNT][HGSS_PAL_TYPE_COUNT] =
                          sSizeScreenSilhouette_inverted_Pal},
 };
 
-#define HGSS_COLOR_MODE HGSS_DARKEST
+#define HGSS_COLOR_MODE HGSS_CLASSIC
 #define SCROLLING_MON_X 146
 #define HGSS_DECAPPED FALSE
 #define HGSS_HIDE_UNSEEN_EVOLUTION_NAMES FALSE
