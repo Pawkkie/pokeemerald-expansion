eaec6be476846940e2c617d23a86a8f0aab568bc
Pawkkie <moog272@gmail.com>
2026-06-01 11:37:55 -0400

    remove comment

--- include/config/ai.h
+++ include/config/ai.h
@@ -90,11 +90,6 @@
 #define AI_IGNORE_BERRY_KO_THRESHOLD                            2   // KO threshold AI must meet in order to treat it berry though it doesn't exist (ie. 2 means "If the AI can 2HKO with berry resisted attack + not-berry resisted next attack, ignore berry resistence when calcing first attack"). Requires AI_DAMAGES_THROUGH_BERRIES
 
 // AI damage calc roll considerations
-// #define AI_ROLL_MIN                                             1
-// #define AI_ROLL_MEDIAN                                          2
-// #define AI_ROLL_MAX                                             3
-// #define AI_ROLL_RANDOM                                          4
-// #define AI_ROLL_TYPE_COUNT                                      5
 enum AiRollDefines
 {
     AI_ROLL_MIN,
